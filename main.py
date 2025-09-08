eb0955db5990520f6311ac15fe1a4bfc135511e3
PAGano-9-Topaz <gano.pa@obmontessori.edu.ph>
2025-09-08 09:07:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# El Me'hon
+from pyscript import display 
+
+
+first_name = 'Josua Ronnel'
+last_name = 'Ortiz'
+age = 15
+weight = 64.6
+is_new_student = False
+sections = ['Topaz', 'Emerald', 'Ruby', 'Sapphire']
+
+display (f'My name is {first_name} {last_name}', target='div1')
